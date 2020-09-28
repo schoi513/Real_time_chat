32113a8df0a02a9efe5dc58423371d2b76f9c6c8
Samuel Choi <schoi@Samuels-MBP.attlocal.net>
2020-09-28 01:52:34 -0500

    added the admin and related messages will be relayed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,18 +11,20 @@ const io = socketio(server);
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'Admin';
+
 // run when client connect
 io.on('connection', socket => {
     
     //Welcome current user
-    socket.emit('message', 'welcome to chatroom');
+    socket.emit('message', formatMessage(botName, 'Welcome to LiveChat'));
 
     //Broadcast when a user connects
-    socket.broadcast.emit('message', 'user has joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName, 'New user has joined the chat'));
 
     //Runs when disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'user has left the chat');
+        io.emit('message', formatMessage(botName, 'User has left the chat'));
     });
 
     //Listen for chatMessage
