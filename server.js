618586ca71ec958d0c4f1dde426620010f1751a1
Samuel Choi <schoi@Samuels-MBP.attlocal.net>
2020-09-28 02:56:57 -0500

    user function added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -16,18 +17,23 @@ const botName = 'Admin';
 // run when client connect
 io.on('connection', socket => {
     socket.on('joinRoom', ({ username, room}) => {
+        const user = userJoin(socket.id, username, room);
+
+        socket.join(user.room);
         
         //Welcome current user
     socket.emit('message', formatMessage(botName, 'Welcome to LiveChat'));
 
     //Broadcast when a user connects
-    socket.broadcast.emit('message', formatMessage(botName, 'New user has joined the chat')
-    );
+    socket.broadcast
+        .to(user.room)
+        .emit('message', formatMessage(botName, `${user.username} has joined the chat`)
+        );
     });
 
     //Runs when disconnects
     socket.on('disconnect', () => {
-        io.emit('message', formatMessage(botName, 'User has left the chat'));
+        io.emit('message', formatMessage(botName, `${user.username} has left the chat`));
     });
 
     //Listen for chatMessage
