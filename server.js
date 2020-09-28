3afd8f995823f6bd38ec3925a35531024dee9e36
Samuel Choi <schoi@Samuels-MBP.attlocal.net>
2020-09-28 01:09:49 -0500

    using socket; welcome, user welcome, disconnect message implemented

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const path = require('path');
 const http = require('http');
-
 const express = require('express');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// run when client connect
+io.on('connection', socket => {
+    
+    //Welcome current user
+    socket.emit('message', 'welcome to chatroom');
+
+    //Broadcast when a user connects
+    socket.broadcast.emit('message', 'user has joined the chat');
+
+    //Runs when disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'user has left the chat');
+    })
+
+});
+
 const PORT = process.env.PORT||3000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
