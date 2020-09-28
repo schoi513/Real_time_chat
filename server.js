6f72b1dcdbbff02a0ffc79cf2d0d6c95a38c4ede
Samuel Choi <schoi@Samuels-MBP.attlocal.net>
2020-09-28 00:57:01 -0500

    hardcoded and style intact, will be changed later

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+const path = require('path');
+const http = require('http');
+
 const express = require('express');
 const app = express();
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT = process.env.PORT||3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
